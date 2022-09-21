0ba9d6f18cd3735414c7a9072b0505b42e81233e
Blackwolf4183 <aperezvic21@gmail.com>
2022-09-21 11:28:31 +0200

    changed again middleware

--- middleware.js
+++ middleware.js
@@ -1,1 +1,23 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
 export { default } from "next-auth/middleware"
+
+export async function middleware(req, res) {
+  // return early if url isn't supposed to be protected
+
+  //console.log("pasa por aqui")
+  /*
+  const token = await getToken({ req, secret: process.env.SECRET });
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/api/auth/unauthorized", req.url));
+  } */
+
+  // If user is authenticated, continue.
+  return NextResponse.next();
+}
+  
+export const config = {
+  matcher: ['/api/addExpenses',"/api/getExpenses","/api/getUser","/api/removeExpense","/api/updateExpense","/api/updateMonthlyLimit"]
+
+}

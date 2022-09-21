e0c81a92ce9f53b51827e3272dc77bb567f1b6f1
Blackwolf4183 <aperezvic21@gmail.com>
2022-09-21 11:44:55 +0200

    Protecting api endpoints

--- pages/api/getExpenses.js
+++ pages/api/getExpenses.js
@@ -1,29 +1,35 @@
 import prisma from "../../lib/prisma";
+import {getSession} from 'next-auth/react'
 
 export default async function handler(req, res) {
 
+  const session = await getSession({req})
 
-
-  if (req.method === "POST") {
-    try {
-      const { email } = req.body;
-
-      let user = await prisma.user.findFirst({
-        where: {
-          email: email,
-        },
-      });
-
-      let expenses = await prisma.expense.findMany({
-        where: {
-          userId: user.id,
-        },
-      });
-
-      expenses = JSON.parse(JSON.stringify(expenses));
-      res.status(200).json(expenses)
-    } catch (err) {
-      res.status(400).json(err);
+  if(!session) res.status(401).json({error: "Not authenticated"})
+  else{
+    if (req.method === "POST") {
+      try {
+        const { email } = req.body;
+  
+        let user = await prisma.user.findFirst({
+          where: {
+            email: email,
+          },
+        });
+  
+        let expenses = await prisma.expense.findMany({
+          where: {
+            userId: user.id,
+          },
+        });
+  
+        expenses = JSON.parse(JSON.stringify(expenses));
+        res.status(200).json(expenses)
+      } catch (err) {
+        res.status(400).json(err);
+      }
     }
   }
+
+  
 }
